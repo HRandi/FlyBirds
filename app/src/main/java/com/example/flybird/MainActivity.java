efdf50ffbe044f2992dc3bf2db8d433102fd209b
Hrandi <hajarandim@gmail.com>
2019-11-24 22:54:21 +0400

    Update de motion censor, Gyroscope & Accelerometre

--- app/src/main/java/com/example/flybird/MainActivity.java
+++ app/src/main/java/com/example/flybird/MainActivity.java
@@ -3,23 +3,31 @@ package com.example.flybird;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GestureDetectorCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imageView;
+    private Accelerometer accelerometer;
     private boolean isMute;
     private GestureDetectorCompat mGestureDetector;
+    private Gyroscope gyroscope;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +42,19 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, GameActivity.class));
             }
         });
+
+        Button btnCamera = findViewById(R.id.btnCamera);
+        ImageView imageView = findViewById(R.id.imageView);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+
 
         TextView highScoreTxt = findViewById(R.id.highScoreTxt);
         final SharedPreferences prefs = getSharedPreferences("game", MODE_PRIVATE);
@@ -63,6 +84,53 @@ public class MainActivity extends AppCompatActivity {
         });
 
         mGestureDetector = new GestureDetectorCompat(this, new GestureListener());
+
+        accelerometer = new Accelerometer(this);
+        gyroscope = new Gyroscope(this);
+
+        accelerometer.setListener(new Accelerometer.Listener() {
+            @Override
+            public void onTranslation(float tx, float ty, float tz) {
+                if (tx > 1.0f) {
+                    getWindow().getDecorView().setBackgroundColor(Color.RED);
+                } else if (tx < -1.0f) {
+                    getWindow().getDecorView().setBackgroundColor(Color.BLUE);
+                }
+
+            }
+        });
+        gyroscope.setListener(new Gyroscope.Listener() {
+            @Override
+            public void onRotation(float rx, float ry, float rz) {
+                if (rz > 1.0f) {
+                    getWindow().getDecorView().setBackgroundColor(Color.GREEN);
+                } else if (rz < -1.0f) {
+                    getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        accelerometer.register();
+        gyroscope.register();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        accelerometer.unregister();
+        gyroscope.unregister();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+        imageView.setImageBitmap(bitmap);
     }
 
     @Override
